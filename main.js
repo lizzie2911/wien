d402b3051f99cd27c4eb872e02cb7f91f38f31a5
Elisabeth Erdozain <lizzie.erdozain@gmail.com>
2025-04-10 15:53:14 +0200

    loadLines style function teal

--- main.js
+++ main.js
@@ -58,7 +58,16 @@ async function loadLines(url) {
     let jsondata = await response.json();
     console.log(jsondata);
     L.geoJSON(jsondata, {
-        attribution: "Datenquelle: <a href='https://data.wien.gv.at' >Stadt Wien </a>"
+        attribution: "Datenquelle: <a href='https://data.wien.gv.at' >Stadt Wien </a>",
+        style: function (feature) {
+            //console.log(feature)
+            return {
+                color: "#39CCCC", 
+                weight: 1,
+                opacity: 0.4,
+                fillOpacity: 0.1,
+            }
+        }
     }).addTo(overlays.lines);
 }
 
